d14402562f11a1c041dddbe7ce7154e64bab3f02
Alyssandro1771415 <dyogo.alyssandro@gmail.com>
2024-03-29 21:25:24 -0300

    feat(Bases de Dados): Adição das bases de dados ao projeto

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
-import Algoritmos.MergeSort;
+import Algoritmos.*;
 
 public class Main {
     public static void main(String[] args) {
         
         int []numbersForTest = {1,2,3,4,6,8,3,7,2,3,1,3};
 
+        MergeSort objeto = new MergeSort();
 
+        objeto.Merge_Sort(numbersForTest);
 
     }
 }
